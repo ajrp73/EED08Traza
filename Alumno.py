13a0d0adc123fd87494fe0a809463a5b987834ec
JUAN FRANCISCO RIVAS RODRIGO <jrivarod@myuax.com>
2025-06-06 13:38:43 +0200

    getters

--- Alumno.py
+++ Alumno.py
@@ -13,3 +13,11 @@ class Alumno:
             __notaMedia= notaMedia
         
         
+    def get_NP(self):
+        return self.__NP
+
+    def get_nombreCompleto(self):
+        return self.__nombreCompleto
+
+    def get_notaMedia(self):
+        return self.__notaMedia
